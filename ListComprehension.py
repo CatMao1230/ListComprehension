11cd5a280e331e6cfd53f711321987d869ab7821
Chia-Chen Yeh <Celine@wonder-iMac.local>
2017-06-28 16:22:16 +0800

    use function generate_list_and_sum to return the answer

--- ListComprehension.py
+++ ListComprehension.py
@@ -1,2 +1,6 @@
-ls = [x ** 2 if x % 2 == 1 else -(x ** 2) for x in range(1, 11)]
-print sum(ls)
+def generate_list_and_sum(start, end):
+	ls = [x ** 2 if x % 2 == 1 else -(x ** 2) for x in range(start, end)]
+	return sum(ls)
+
+if __name__ == '__main__':
+	print generate_list_and_sum(1, 11)
